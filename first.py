2b6a16ba9f7176d42aec4ae3a3c0069e4b6b1724
Brahim <68456555+brahimHAMAMA@users.noreply.github.com>
2023-03-13 14:51:22 +0300

    lesson 01 - 05

---
+++ first.py
@@ -0,0 +1,13 @@
+print("Hello Python")
+print("Hello Programming")
+print("Hello Programming")
+# print("Hello Programming")
+
+""""
+print("Hello Programming")
+ssfsf
+sfsdsfs
+sdsdsd
+sdsds
+
+"""
